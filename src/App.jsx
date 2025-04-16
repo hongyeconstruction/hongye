fb730e654707301f074600d96ade4bc59dce5134
Hao <Hao@michelles-imac.home>
2025-04-15 23:14:04 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Services from "./components/Services";
 import Profession from "./components/Profession";
 // import Contact from "./components/Contact";
 import Gallery from "./components/Gallery";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -25,9 +25,9 @@ function App() {
         {/* <Contact /> */}
       </div>
       <Gallery />
-      {/* <div id="footer" className="footer">
+      <div id="footer" className="footer">
         <Footer />
-      </div> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import "../styles/Footer.css";
+import Icons from "../Icons";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="footer-corp-info">
+        <div className="footer-logo-name">
+          <div className="footer-corp-logo"></div>
+          <div className="footer-corp-name">
+            Hongye Landscaping Construction & Construction
+          </div>
+        </div>
+        <ul className="footer-service">
+          <li>Service1</li>
+          <li>Service2</li>
+          <li>Service3</li>
+          <li>Service4</li>
+        </ul>
+      </div>
+      <div className="footer-contact">
+        <div className="footer-contact-title">Get In Touch</div>
+        <div className="footer-contact-email">Haobo1012@gmail.com</div>
+        <div className="footer-contact-phone">647-780-0008</div>
+        <div className="footer-contact-wechat"></div>
+      </div>
+      <div className="footer-links">
+        <div className="footer-links-title">Quick Links</div>
+        <div>
+          <a href="#homepage">
+            <Icons.Triangle />
+            Homepage
+          </a>
+        </div>
+        <div>
+          <a href="#aboutus">
+            <Icons.Triangle />
+            About Us
+          </a>
+        </div>
+        <div>
+          <a href="#service">
+            <Icons.Triangle />
+            Our Services
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <Icons.Triangle />
+            Our Pricing
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <Icons.Triangle />
+            Contact Us
+          </a>
+        </div>
+
+        <div className="SocialIcons">
+          <Icons.Email />
+          <Icons.Wechat />
+          <Icons.Call />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
